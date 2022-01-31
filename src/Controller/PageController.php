e089a25e53869066782fdebfb1d31fd2415ca3c7
sergey-lisitsyn <sergej.lisitsyn@gmail.com>
2022-01-31 13:23:32 +0200

    remove unused use statement

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use App\Repository\PageRepository;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
